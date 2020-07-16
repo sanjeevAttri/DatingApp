f1f6b8e72c0581ecd320e676bc87c1190fc32873
Sanjeev Sharma <sanjeevattri2909@gmail.com>
2020-07-17 04:15:24 +0530

    Client side login and register

---
+++ DatingAppSPA/src/app/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  //values: any;
+
+  constructor(private Http: HttpClient) { }
+
+  ngOnInit() {
+    //this.getValues();
+  }
+
+  registerToggel(){
+      this.registerMode = true;
+  }
+
+  // getValues(){
+  //   this.Http.get('http://localhost:5000/api/values').subscribe(response => {
+  //     this.values = response;
+  //   }, error =>{
+  //   console.log(error);
+  //   });
+  // }
+
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode= registerMode;
+  }
+
+}
